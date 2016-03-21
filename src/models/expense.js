1f6a957a5c2068ac17db895e0ba5fa2878335c23
Albin George <albinin0002@gmail.com>
2016-03-22 03:20:42 +0530

    Added expense model

---
+++ src/models/expense.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+// create a schema
+var expenseSchema = new Schema({
+    id: ObjectId,
+    short_desc: { type: String, required: true, trim: true },
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    amount: {
+        type: Number,
+        get: getPrice,
+        set: setPrice,
+        required: true
+    },
+    desc: String,
+    created_at: Date,
+    updated_at: Date
+});
+
+expenseSchema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
+});
+
+function getPrice(num){
+    return (num/100).toFixed(2);
+}
+
+function setPrice(num){
+    return num*100;
+}
+
+
+var Expense = mongoose.model('Expense', expenseSchema);
+
+module.exports = Expense;
